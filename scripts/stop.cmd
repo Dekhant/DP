4456201c08fc9ca98fd259ecac4aa23c3f437617
Jurmmander <52196622+Jurmmander@users.noreply.github.com>
2021-03-24 15:17:18 +0300

    fix stop script

--- scripts/stop.cmd
+++ scripts/stop.cmd
@@ -1,6 +1,12 @@
 docker stop redis & docker rm redis
 docker stop nats & docker rm nats
 
+set NGINX_PATH="../nginx-1.19.7/"
+
 taskkill /f /im valuator.exe
-taskkill /f /im nginx.exe
 taskkill /f /im rankcalculator.exe
+pushd %NGINX_PATH%
+
+nginx -s stop
+
+popd
