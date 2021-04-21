0051c613b90832d50eb3d97ecbbcc06777efeebb
Dekhant <sasha.dekhant@gmail.com>
2021-04-21 18:16:47 +0300

    issues

--- scripts/stop.cmd
+++ scripts/stop.cmd
@@ -1,12 +1,8 @@
 docker stop redis & docker rm redis
 docker stop nats & docker rm nats
 
-set NGINX_PATH="../nginx-1.19.7/"
-
 taskkill /f /im valuator.exe
 taskkill /f /im rankcalculator.exe
-pushd %NGINX_PATH%
 
+cd ..\nginx\
 nginx -s stop
-
-popd
