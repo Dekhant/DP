f68658303af2761acbeabd79695fa5efd90f16a0
Dekhant <sasha.dekhant@gmail.com>
2021-06-05 11:01:39 +0300

    add lab 7

---
+++ Chain/Interactions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Chain
+{
+    public static class Interactions
+    {
+        public static int ReceiveInt(this Socket socket)
+        {
+            var buf = new byte[sizeof(int)];
+            socket.Receive(buf);
+            return BitConverter.ToInt32(buf);
+        }
+
+        public static void SendInt(this Socket socket, int number)
+        {
+            socket.Send(BitConverter.GetBytes(number));
+        }
+    }
+}
